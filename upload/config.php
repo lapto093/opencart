9690f9c51401ad3a1529b37bc79d1d9b2d46a057
lapto093 <lapto093@gmail.com>
2014-04-11 18:01:16 +0700

    addtest

---
+++ upload/config.php
@@ -0,0 +1,26 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.opencart-demo.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.opencart-demo.com/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\Projects\Opencart\Sample3\upload/catalog/');
+define('DIR_SYSTEM', 'D:\Projects\Opencart\Sample3\upload/system/');
+define('DIR_LANGUAGE', 'D:\Projects\Opencart\Sample3\upload/catalog/language/');
+define('DIR_TEMPLATE', 'D:\Projects\Opencart\Sample3\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\Projects\Opencart\Sample3\upload/system/config/');
+define('DIR_IMAGE', 'D:\Projects\Opencart\Sample3\upload/image/');
+define('DIR_CACHE', 'D:\Projects\Opencart\Sample3\upload/system/cache/');
+define('DIR_DOWNLOAD', 'D:\Projects\Opencart\Sample3\upload/system/download/');
+define('DIR_MODIFICATION', 'D:\Projects\Opencart\Sample3\upload/system/modification/');
+define('DIR_LOGS', 'D:\Projects\Opencart\Sample3\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
