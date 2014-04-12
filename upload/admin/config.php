f1f765c7a58d31ed0bdf6f6d8c88c992a23bc73f
unknown <lapto093@gmail.com>
2014-04-11 19:55:52 -0700

    add project opencart

---
+++ upload/admin/config.php
@@ -0,0 +1,29 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.opencart-demo.com/admin/');
+define('HTTP_CATALOG', 'http://local.opencart-demo.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.opencart-demo.com/admin/');
+define('HTTPS_CATALOG', 'http://local.opencart-demo.com/');
+
+// DIR
+define('DIR_APPLICATION', 'G:\Technical\Opencart\Demo\upload/admin/');
+define('DIR_SYSTEM', 'G:\Technical\Opencart\Demo\upload/system/');
+define('DIR_LANGUAGE', 'G:\Technical\Opencart\Demo\upload/admin/language/');
+define('DIR_TEMPLATE', 'G:\Technical\Opencart\Demo\upload/admin/view/template/');
+define('DIR_CONFIG', 'G:\Technical\Opencart\Demo\upload/system/config/');
+define('DIR_IMAGE', 'G:\Technical\Opencart\Demo\upload/image/');
+define('DIR_CACHE', 'G:\Technical\Opencart\Demo\upload/system/cache/');
+define('DIR_DOWNLOAD', 'G:\Technical\Opencart\Demo\upload/system/download/');
+define('DIR_LOGS', 'G:\Technical\Opencart\Demo\upload/system/logs/');
+define('DIR_MODIFICATION', 'G:\Technical\Opencart\Demo\upload/system/modification/');
+define('DIR_CATALOG', 'G:\Technical\Opencart\Demo\upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
---
+++ upload/config.php
@@ -0,0 +1,26 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://local.opencart-demo.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://local.opencart-demo.com/');
+
+// DIR
+define('DIR_APPLICATION', 'G:\Technical\Opencart\Demo\upload/catalog/');
+define('DIR_SYSTEM', 'G:\Technical\Opencart\Demo\upload/system/');
+define('DIR_LANGUAGE', 'G:\Technical\Opencart\Demo\upload/catalog/language/');
+define('DIR_TEMPLATE', 'G:\Technical\Opencart\Demo\upload/catalog/view/theme/');
+define('DIR_CONFIG', 'G:\Technical\Opencart\Demo\upload/system/config/');
+define('DIR_IMAGE', 'G:\Technical\Opencart\Demo\upload/image/');
+define('DIR_CACHE', 'G:\Technical\Opencart\Demo\upload/system/cache/');
+define('DIR_DOWNLOAD', 'G:\Technical\Opencart\Demo\upload/system/download/');
+define('DIR_MODIFICATION', 'G:\Technical\Opencart\Demo\upload/system/modification/');
+define('DIR_LOGS', 'G:\Technical\Opencart\Demo\upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
